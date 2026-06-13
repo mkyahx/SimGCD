e4a0c61c174de97d0dc17a4d5c1ae14904cfe4c0
Shenru Zhang <mkyahx@connect.hku.hk>
2026-06-13 12:49:19 +0800

    update on config

--- config.py
+++ config.py
@@ -3,9 +3,9 @@
 # -----------------
 cifar_10_root = '${DATASET_DIR}/cifar10'
 cifar_100_root = '${DATASET_DIR}/cifar100'
-cub_root = '${DATASET_DIR}/cub'
-aircraft_root = '${DATASET_DIR}/fgvc-aircraft-2013b'
-car_root = '${DATASET_DIR}/cars'
+cub_root = '/lustre1/g/stat_han/datasets/cub'
+aircraft_root = '/lustre1/g/stat_han/datasets/fgvc-aircraft-2013b'
+car_root = '/lustre1/g/stat_han/datasets/cars'
 herbarium_dataroot = '${DATASET_DIR}/herbarium_19'
 imagenet_root = '${DATASET_DIR}/ImageNet'
 
@@ -15,4 +15,4 @@ osr_split_dir = 'data/ssb_splits'
 # -----------------
 # OTHER PATHS
 # -----------------
-exp_root = 'dev_outputs' # All logs and checkpoints will be saved here
+exp_root = '/home/mkyahx/SimGCD/dev_outputs' # All logs and checkpoints will be saved here
